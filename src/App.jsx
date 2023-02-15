732aea902f3f3b1ef4ec39ced93919023f4e33c3
VickyChang <a0197391@gmail.com>
2023-02-16 01:27:00 +0800

    add icon

--- src/App.jsx
+++ src/App.jsx
@@ -100,7 +100,7 @@ function App() {
             臺北市和平東路二段134號，國立臺北教育大學 數位科技設計學系
           </h5>
           <ul className="footer__list col-lg-4">
-            <li className="footer__item"><a href="" className="footer__link"><i className="fab fa-facebook-f"></i></a></li>
+            <li className="footer__item"><a href="" className="footer__link"><FontAwesomeIcon icon={faFacebookF} /></a></li>
             <li className="footer__item"><a href="" className="footer__link"><i className="fab fa-google-plus-g"></i></a></li>
             <li className="footer__item"><a href="" className="footer__link"><i className="fab fa-linkedin-in"></i></a></li>
           </ul>
