4047ccfcb937c6243a3925cc2715929f2ef5666c
VickyChang <a0197391@gmail.com>
2023-03-13 03:40:15 +0800

    antd+CSS Grid+CSS Module

--- src/assets/Pages/Home.jsx
+++ src/assets/Pages/Home.jsx
@@ -1,21 +1,3 @@
-// import Header from "../components/Header/Header";
-// import Footer from "../components/Footer/Footer";
-// import PictureList from "../components/PictureList/PictureList";
-
-// function Home(){
-//     return(
-//         <div className="container main-layout">
-//            <Header className="layout-header" />
-//            <PictureList className="layout-content" />
-//            <Footer className="layout-footer" />
-//         </div>
-//     )
-
-// }
-
-// export default Home;
-
-
 import Header from "../components/Header";
 import PictureList from "../components/PictureList/ImageList";
 import Description from "../components/Description";
