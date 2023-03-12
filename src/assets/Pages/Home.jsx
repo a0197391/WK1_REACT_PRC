9154909e817c1d70d1d3200e60f3618701b7e74c
VickyChang <a0197391@gmail.com>
2023-03-13 03:31:59 +0800

    test pic

--- src/assets/Pages/Home.jsx
+++ src/assets/Pages/Home.jsx
@@ -6,7 +6,7 @@ function Home(){
     return(
         <div className="container main-layout">
            <Header className="layout-header" />
-           <PictureList className="layout-content" />
+           {/* <PictureList className="layout-content" /> */}
            {/* <Footer className="layout-footer" /> */}
         </div>
     )
