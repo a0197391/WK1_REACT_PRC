28c9ff75bbb9d5d9e67de06137e704f1ea91ead0
VickyChang <a0197391@gmail.com>
2023-03-13 03:37:45 +0800

    add antd

--- src/assets/Pages/Home.jsx
+++ src/assets/Pages/Home.jsx
@@ -1,16 +1,38 @@
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
-import PictureList from "../components/PictureList/PictureList";
+// import Header from "../components/Header/Header";
+// import Footer from "../components/Footer/Footer";
+// import PictureList from "../components/PictureList/PictureList";
 
-function Home(){
-    return(
-        <div className="container main-layout">
-           <Header className="layout-header" />
-           {/* <PictureList className="layout-content" /> */}
-           {/* <Footer className="layout-footer" /> */}
+// function Home(){
+//     return(
+//         <div className="container main-layout">
+//            <Header className="layout-header" />
+//            <PictureList className="layout-content" />
+//            <Footer className="layout-footer" />
+//         </div>
+//     )
+
+// }
+
+// export default Home;
+
+
+import Header from "../components/Header";
+import PictureList from "../components/PictureList/ImageList";
+import Description from "../components/Description";
+import Footer from "../components/Footer";
+import imgs from "../json/pictures.json"
+
+function Home() {
+    return (
+        <div className="mainLayout">
+            <Header className="layoutHeader" />
+            <div className="layoutContent" >
+                <PictureList img={imgs} />
+                <Description />
+            </div>
+            <Footer className="layoutFooter" />
         </div>
-    )
-
+    );
 }
 
 export default Home;
