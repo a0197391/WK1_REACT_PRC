fa702ca3f8dcee2ef7036d7afe3474021ca46219
VickyChang <a0197391@gmail.com>
2023-04-29 20:21:47 +0800

    add isLoading

--- src/assets/Pages/Home.jsx
+++ src/assets/Pages/Home.jsx
@@ -2,15 +2,21 @@ import Header from "../components/Header";
 import PictureList from "../components/PictureList";
 import Description from "../components/Description";
 import Footer from "../components/Footer";
+
+import { useImages } from "../react-query";
 // import pictures from "../json/pictures.json"
 
 function Home() {
+    const { data, isLoading } = useImages();
+    const imgs = data || [];
+
+
     return (
         <div className="mainLayout">
             <Header className="layoutHeader" />
             <div className="layoutContent" >
                 {/* <PictureList img={pictures} /> */}
-                <PictureList/>
+                <PictureList img={imgs}/>
                 <Description />
             </div>
             <Footer className="layoutFooter" />
