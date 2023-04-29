d62706e64732e643a20c9d1637f14af2eefc6264
VickyChang <a0197391@gmail.com>
2023-04-29 16:37:54 +0800

    back org

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import './App.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import 'antd/dist/reset.css'
 import './App.css'
 import Home from './assets/Pages/Home'
 
-import { Provider } from "react-redux";
-import Router from './Router';
-import { persistor, store } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+
 
 
 const queryClient = new QueryClient()
@@ -17,15 +14,7 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    // <Home />
-    // <QueryClientProvider client={queryClient}>
-    //   <Home />
-    // </QueryClientProvider>
-    <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Router />
-    </PersistGate>
-  </Provider>
+    <Home />
   );
 }
 
