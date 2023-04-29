d8144c6223bcb56041840f740d8ea5ca85c1f622
VickyChang <a0197391@gmail.com>
2023-04-29 16:07:09 +0800

    try firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import './App.css'
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'antd/dist/reset.css'
 import './App.css'
-
 import Home from './assets/Pages/Home'
 
+
+const queryClient = new QueryClient()
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <Home />
+    // <Home />
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
   );
 }
 
