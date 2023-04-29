a50adeb870babb08ee5454664964a302910ec563
VickyChang <a0197391@gmail.com>
2023-04-29 16:44:31 +0800

    back

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import './App.css'
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'antd/dist/reset.css'
 import './App.css'
 import Home from './assets/Pages/Home'
+
+// import { Provider } from "react-redux";
+// import Router from './Router';
+// import { persistor, store } from './redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
+
 
 function App() {
   // const [count, setCount] = useState(0)
