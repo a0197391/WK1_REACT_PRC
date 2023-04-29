31cb85a8950d41b312f218ddd5783e52aa86e7e1
VickyChang <a0197391@gmail.com>
2023-04-29 16:40:32 +0800

    delete firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import './App.css'
-
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import 'antd/dist/reset.css'
 import './App.css'
 import Home from './assets/Pages/Home'
 
-
+// import { Provider } from "react-redux";
+// import Router from './Router';
+// import { persistor, store } from './redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
 
 
 const queryClient = new QueryClient()
